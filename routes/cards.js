5e0f4aff5ebd195e3c2e2a1faefed462e7eaaef0
Vitaliy Sokolov <vitalysokolov9751@gmail.com>
2019-12-06 18:33:30 +0400

    New commit2

--- routes/cards.js
+++ routes/cards.js
@@ -6,8 +6,8 @@ cards.get('/', async (_req, res) => {
   try {
     const data = await readDataFromFile(path.resolve(__dirname, '../data/cards.json'));
     return res.send(JSON.parse(data));
-  } catch (e) {
-    return res.status(500).send({ message: 'Can`t read from cards.json' });
+  } catch (error) {
+    return res.status(500).send({ message: error });
   }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -6,26 +6,27 @@ users.get('/', async (req, res) => {
   try {
     const data = await readDataFromFile(path.resolve(__dirname, '../data/users.json'));
     return res.send(JSON.parse(data));
-  } catch (e) {
-    return res.status(500).send({ message: 'Can`t read from users.json' });
+  } catch (error) {
+    return res.status(500).send({ message: error });
   }
 });
 
 // eslint-disable-next-line consistent-return
 users.get('/:id', async (req, res) => {
   const { id } = req.params;
-  let data = await readDataFromFile(path.resolve(__dirname, '../data/users.json'));
   try {
+    // eslint-disable-next-line no-unused-vars
+    let data = await readDataFromFile(path.resolve(__dirname, '../data/users.json'));
     data = Object.values(JSON.parse(data)).map((value) => Object.values(value));
-  } catch (e) {
-    return res.status(500).send({ message: 'Bad users.json' });
-  }
-  // eslint-disable-next-line no-shadow
-  const user = data.find((user) => user.id === id);
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send({ message: `Нет пользователя с id ${id}` });
+    // eslint-disable-next-line no-shadow
+    const user = data.find((user) => user.id === id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: `Нет пользователя с id ${id}` });
+    }
+  } catch (error) {
+    return res.status(500).send({ message: error });
   }
 });
 
